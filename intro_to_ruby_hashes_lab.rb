d10c3b84f73d9b21c40ff5f72bde5f9362d9e003
Tish Chungoora <tishal82@gmail.com>
2019-09-17 09:56:29 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -1,29 +1,45 @@
+# Creates an empty hash and returns it
+
 def new_hash
-  # return an empty hash
+  Hash.new
 end
+
+# Creates and returns a valid hash that contains key/value pairs of your choice
 
 def my_hash
-  # return a valid hash with any key/value pair of your choice
+  {:my_key => "my_value"}
 end
+
+# Creates and returns a hash named pioneer whose key is a symbol :name and whose value is a string, 'Grace Hopper'
 
 def pioneer
-  # return a hash with a key of :name and a corresponding value of 'Grace Hopper'
+  {:name => "Grace Hopper"}
 end
+
+# Creates and returns a hash with a key :id and a random number assigned to the value
 
 def id_generator
-  # return a hash with a key :id assigned to positive integer
+  {:id => 5}
 end
 
+# Accepts a key and a value as parameters and returns a hash with this key/value pair inside
 def my_hash_creator(key, value)
-  # return a hash that includes the key and value parameters passed into this method
+  hash[key] = value
 end
+
+# Returns the value corresponding to the provided key
 
 def read_from_hash(hash, key)
-  # return the correct value using the hash and key parameters
+  hash[key]
 end
 
+# If key is present, returns a hash where the key's value is incremented by 1
+# If key is not present, returns a hash with the provided key assigned to the value of 1
+
 def update_counting_hash(hash, key)
-  # given a hash an a key as parameters, return an updated hash
-  # if the provided key is not present in the hash, add it and assign it to the value of 1
-  # if the provided key is present, increment its value by 1
+  if hash[key]
+    hash[key] += 1
+  else
+    hash[key] = 1
+  end
 end
